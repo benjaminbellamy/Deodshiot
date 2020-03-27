546da501d582f4619b3f2af858fb00d8b86dc2e6
Ben <benjamin.bellamy@gmail.com>
2020-03-27 19:49:44 +0100

    Updated code

--- Deodshiot/Deodshiot.ino
+++ Deodshiot/Deodshiot.ino
@@ -13,11 +13,17 @@
 #define MAX_FREQUENCY1 30000      // How often (30s) do we allow triggering
 #define MAX_FREQUENCY2 300000     // How often (5mn) do we allow triggering
 #define MAX_FREQUENCY3 1800000    // How often (30mn) do we allow triggering
+#define MIN_DURATION1 5000        // How long (5s) should the light stay on to allow triggering
+#define MIN_DURATION2 180000      // How long (3mn) should the light stay on to allow triggering
+#define MIN_DURATION3 300000      // How long (5mn) should the light stay on to allow triggering
 #define MOTOR_DELAY 500           // How long will the motor stay on 
 
 int sensorValues[NUM_VAL];        // create an array for sensor data
 int i=0;
-unsigned long lastTime = 0-MAX_FREQUENCY3;
+unsigned long lastTime = millis()-MAX_FREQUENCY3;
+unsigned long switchOnTime = millis();
+boolean isLightOn=false;
+
 
 // Reset all values in array :
 void resetValues(){
@@ -41,22 +47,38 @@ void loop() {
   // read the input on analog pin:
   int sensorValue = analogRead(SENSOR_PIN);
 
-  // Sets the frequency depending on the sliding switch:
+  // Sets the frequency and duration depending on the sliding switch:
   unsigned long maxFrequency = MAX_FREQUENCY1;
+  unsigned long minDuration = MIN_DURATION1;
   if(digitalRead(SWITCH2_PIN)){
     maxFrequency = MAX_FREQUENCY2;
+    minDuration = MIN_DURATION2;
   } else if(digitalRead(SWITCH3_PIN)){
     maxFrequency = MAX_FREQUENCY3;
+    minDuration = MIN_DURATION3;
   }
 
-  // if the new value is lower that the old one, trigger is raised:
-  if(sensorValues[i]-sensorValue > THRESHOLD && (millis()-lastTime)>maxFrequency){
-      digitalWrite(RELAY_PIN, HIGH);
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(500);
-      digitalWrite(RELAY_PIN, LOW);
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  // Switch ON
+  // if the new value is higher that the old one, we store the current time:
+  if(sensorValue-sensorValues[i] > THRESHOLD){
       lastTime = millis();
+      switchOnTime = lastTime;
+      isLightOn=true;
+      resetValues();
+  }
+
+  // Switch OFF
+  // if the new value is lower that the old one, trigger is raised:
+  if(isLightOn && sensorValues[i]-sensorValue > THRESHOLD){
+      if((millis()-lastTime)>maxFrequency && (millis()-switchOnTime)>minDuration){
+        digitalWrite(RELAY_PIN, HIGH);     // turn the motor on
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on
+        delay(500);
+        digitalWrite(RELAY_PIN, LOW);     // turn the motor off
+        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off
+      }
+      lastTime = millis();
+      isLightOn=false;
       resetValues();
   }
 
